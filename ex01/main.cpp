6a3cf1d3f57b5ea0133c5b852acf3d4b554f7bdd
Hoppy <mbraets@student.42.fr>
2022-08-13 15:57:28 +0200

    ex01 - better log and main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 #include <stdint.h>
 
+#define PURPLE(A) "\033[35m" << A << "\033[0m"
+
 struct Data
 {
 	std::string	name;
 	int			age;
 };
-
-
 
 uintptr_t serialize(Data* ptr)
 {
@@ -21,14 +21,26 @@ Data* deserialize(uintptr_t raw)
 
 int	main(void)
 {
-	Data a = Data();
-	a.name	= "Bob";
-	a.age	= 20;
+	Data before;
+	before.name	= "Bob";
+	before.age	= 21;
 
-	uintptr_t raw = serialize(&a);
-	Data *b = deserialize(raw);
+	uintptr_t raw = serialize(&before);
+	Data *after = deserialize(raw);
+
+	std::cout	<< PURPLE("Address of Data before serialize:  ") << &before << std::endl
+				<< PURPLE("Address of Data after deserialize: ") << after  << std::endl;
+
+	std::cout << std::endl;
 
 	std::cout
-	<< b->name << std::endl
-	<< b->age << std::endl;
+	<< PURPLE("Data before.Name: ") << before.name << std::endl
+	<< PURPLE("Data before.Age: ")  << before.age  << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout
+	<< PURPLE("Data after.Name: ") << after->name << std::endl
+	<< PURPLE("Data after.Age: ")  << after->age  << std::endl;
+
 }
